d9796f25e74029e1270161dff80c3f0d0fd13920
elankathir1270 <elankathirskr92@gmail.com>
2023-02-12 10:06:23 +0530

    2nd commit

---
+++ Mark_Entry/src/org/mark/FINALMAIN.java
@@ -0,0 +1,13 @@
+package org.mark;
+
+public class FINALMAIN {
+	
+	public static void main(String[] args) {
+		
+		Mark mark=new Mark();
+		mark.markEntry();
+		
+		
+	}
+
+}
--- Mark_Entry/src/org/mark/Mark.java
+++ Mark_Entry/src/org/mark/Mark.java
@@ -1,13 +1,14 @@
 package org.mark;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class Mark {
 	
-	public static void main(String[] args) {
+	public boolean markEntry() {
 		int sid=0;
-		int tc=0;
+		
 		for (int k = 0; k < 2; k++) {
 			sid++;
 			System.out.println("=========================");
@@ -19,8 +20,6 @@ public class Mark {
 			id++;
 			System.out.println("student id: "+"SRK00"+id+"\n"+"Enter the Marks (lan1,lan2,mat,sci,soc)");
 			
-			
-		
 		int marks[]=new int[5];
 		String sub[]= {"lan1","lan2","mat","sci","soc"};
 		String print[]=new String[sub.length];
@@ -30,8 +29,14 @@ public class Mark {
 			
 			 marks[i]=sc.nextInt();
 			 
-			print[i]=sub[i]+"-"+marks[i];
+			 if(marks[i]>100||marks[i]<0) {
+			 System.out.println("enter valid marks");
+			 return false;
+			 }
+			 print[i]=sub[i]+"-"+marks[i];
+		
 		}
+				
 		int total=0;
 		int avg=0;
 		int cnt=0;
@@ -53,19 +58,21 @@ public class Mark {
 			
 			total=total+marks[i];
 			avg=total/cnt;
-			tc++;
+			
 		}
+		
 		System.out.println();
 		System.out.println("total: "+total+"/500");
 		System.out.println("percentage %: "+avg);
 		System.out.println("pass in: "+pass+" subjects"+"|"+"fail in: "+fail+" subjects"+"|"+"ton in: "+ton+" subjects");
 		System.out.println();
+		
 		}
 	}
-		System.out.println(tc=tc/5);
-	
-			
-		}
+		return true;
+		
+		
+	}
 	
 	}
 
